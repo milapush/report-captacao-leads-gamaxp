e488296b35577d0344c6aaa3f9ce0144028fa34a
Camila Gomes <camila.s.gomes@hotmail.com>
2020-08-21 01:47:20 -0300

    Adicionar função exemplo de redirecionamento

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// function goToHacker() {
+//         window.location.href = 'hacker.html';
+// }
+
+// function goToHipster() {
+//         window.location.href = 'hipster.html';
+// }
+
+// function goToHustler() {
+//         window.location.href = 'hustler.html';
+// }
+
+// function goToHyper() {
+//         window.location.href = 'hyper.html';
+// }
+
+// function goToAboutUs() {
+//         window.location.href = 'sobre-nos.html';
+// }
+
+// function goToIndex() {
+//         window.location.href = 'index.html';
+// }
+
+// // COLOCAR ISSO NO HTML 
+
+// <div class="banner">
+//         <h1>OLA MENINAS, TUDO BOM?</h1>
+//         <button type="button" class="btn btn-primary add-task" onclick="Nova();">Beneficiário</button>
+// </div>
